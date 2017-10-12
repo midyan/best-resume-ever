4f7c3701590aefbb464cbcb279e8947c18576946
yannunes <yannunes@gmail.com>
2017-10-12 18:37:05 -0300

    personal info

--- src/person.js
+++ src/person.js
@@ -1,54 +1,102 @@
 /* eslint-disable */
 export const PERSON = {
   name: {
-    first: 'John',
-    middle: '',
-    last: 'Doe',
+    first: 'Yan',
+    middle: 'Mascarenhas',
+    last: 'Nunes',
   },
-  about: 'Hi, my name is John Doe. I\'m just about the most boring type of person you could possibly imagine. I like collecting leaves from the tree in my back yard and documenting each time I eat a peanut that is non-uniform. I am not a robot. Please hire me.',
-  position: 'Software Developer',
+  about: "I'm a Full Stack Developer with an unending hunger for knowledge and unrivaled love for JavaScript. I have experience building high quality web-applications and website, as well as building cross-platform Mobile Apps. I like to work in a team that is very passionate about their work and cares about how the user will interact with the final product. Responsive designs with a mobile-first mindset are my main focus when creating a UI.",
+  position: 'FullStack Developer',
   birth: {
-    year: 1990,
-    location: 'New York'
+    year: 1994,
+    location: 'Salvador'
   },
   experience: [{
-      company: 'Company A',
-      position: 'Developer',
-      timeperiod: 'since January 2016',
-      description: 'Programming and watching cute cat videos.'
+      company: 'Apinf Oy',
+      position: 'Software Engineer',
+      timeperiod: 'since August 2017',
+      description: `About APinf:
+        In the technology driven world of digital transformation and IoT, we at APInf still believe that developers are, and they should be, lazy. Develop, we do the REST. APInf.io is a pioneering open source platform, which offers comprehensive yet easy to use API management tool. Our goal was to build a bridge between API owners and consumers and help them to find each other easier. APInf.io provides simplified workflow for API owners for common API management tasks. At the same time, it serves as a self-service tool for API consumers for discovering APIs and taking them to use. Role description:
+        Currently, working remotely for APInf. I am a Software Engineer involved in all software projects of the company.
+        \n
+        Projects:
+        - APInf Platform (Meteor.JS) - I have helped improving the code quality through automated testing and continuous integration. I also helped implementing SSO authentication flow to integrate the Apinf Platform with FIWARE IDM Service.
+        \n
+        Technologies used:
+        ☑ MQTT
+        ☑ MongoDB
+        ☑ Travis-ci
+        ☑ Jest
+        ☑ Nock
+        ☑ Docker
+        ☑ OAuth2`
     },
     {
-      company: 'Company B',
-      position: 'Frontend Developer',
-      timeperiod: 'January 2015 - December 2015',
-      description: 'Fulfillment of extremely important tasks.'
+      company: 'ABEPRO',
+      position: 'Mobile Applications Developer',
+      timeperiod: 'since May 2016',
+      description: `ABEPRO is an institution which represents teachers, students and professionals of Production Engineering.
+      The association was founded more than 25 years ago and has the following functions: to clarify the role of the Production Engineer in the society and in his/her market, to be a link to the government institutes related to organizing and evaluating (MEC e INEP) and supportting courses (Capes, CNPq , Finep and entities which support regional researches), as well as private organizations as CREA , Confea, SBPC, Abenge and others non-governmental organizations which deal with research, teaching and extension of engineering.
+
+      Role description:
+      - Enegep 2017 App (React Native) - I have developed an offline first, realtime-update App for the ENEGEP 2017, the largest Production Engineering Congress in Brazil! The app will help users navigate through the event schedule, allow them to evaluate the lectures and make networking easier and more effective.
+
+      Technologies used:
+      ☑ Javascript
+      ☑ React Native
+      ☑ Push Notifiations
+      ☑ App Store
+      ☑ Play Store
+      ☑ ES6
+      ☑ WebSocket
+      ☑ Pub/Sub
+      ☑ Web Application Messaging Protocol (WAMP)
+      ☑ MongoDB`
     },
     {
-      company: 'Company C',
-      position: 'Trainee',
-      timeperiod: 'March 2014 - December 2014',
-      description: 'Making coffee and baking cookies.'
+      company: 'Tag.mx',
+      position: 'Full Stack Developer',
+      timeperiod: 'since september 2016',
+      description: `AboutTag.MX:
+      Tag.MX is a Digital Agency that relies on bleeding edge technologies to create highly dynamic and customizable systems for the B2B and B2C marketplace. We develop solutions ranging from Website, WebSystems and Graphical Desgint to IT infrastructure service and IT consulting.
+
+      Role Description:
+      Responsible for leading the core development team of the company's flagship product, a full-fledged ERP(Enterprise Resource Planning).
+
+      Projects:
+      ERP(Enterprise Resource Planning) - Full-fledged ERP(Enterprise Resource Planning) that is a dynamic real-time and robust application for warehouse control, sales and accountings management, as well as a CRM platform. It is capable of summarizing the entire management of a company to a single system, helping to reduce the operations cost by a significant amount.
+
+      Technologies Used:
+      ☑ Javascript
+      ☑ AngularJS
+      ☑ Node.js
+      ☑ Mocha
+      ☑ Mongoose
+      ☑ MongoDB
+      ☑ WebSocket
+      ☑ WAMP
+      ☑ Crossbar.io
+      ☑ Bootstrap
+      ☑ Nifty
+      ☑ Gulp
+      ☑ NPM`
     }
   ],
   education: [{
-      degree: 'Master of Arts',
-      timeperiod: 'March 2012 - December 2013',
-      description: 'Major in Hacking and Computer Penetration, University A, New York, USA.'
+      degree: 'SENAI CIMATEC',
+      timeperiod: 'July 2012 - July 2014',
+      description: `This course aims to graduate very capable professionals to work as Mechatronics Technicians. Real life problems are a core part of the course curriculum, ensuring that all students have both theoretical and practical experience when they leave the institution.`
     },
-    {
-      degree: 'Bachelor of Science',
-      timeperiod: 'March 2009 - December 2011',
-      description: 'Major in Engineering, University B, Los Angeles, USA.'
-    }
   ],
   // skill level goes 0 to 100
-  skills: [{
+  skills: [
+    {
       name: 'HTML5',
-      level: '99'
+      level: '90'
     },
     {
       name: 'CSS3',
-      level: '95'
+      level: '72'
     },
     {
       name: 'JavaScript',
@@ -59,30 +107,51 @@ export const PERSON = {
       level: '93'
     },
     {
-      name: 'Angular 2',
-      level: '60'
+      name: 'Google Cloud Platform',
+      level: '85'
     },
     {
-      name: 'TypeScript',
+      name: 'Git',
+      level: '81'
+    },
+    {
+      name: 'Jest',
+      level: '94'
+    },
+    {
+      name: 'TravisCI',
+      level: '86'
+    },
+    {
+      name: 'React',
+      level: '90'
+    },
+    {
+      name: 'React Native',
+      level: '90'
+    },
+    {
+      name: 'Angular',
       level: '80'
     },
     {
-      name: 'ES.Next',
-      level: '70'
+      name: 'Yarn',
+      level: '90'
     },
     {
-      name: 'Docker',
-      level: '99'
+      name: 'ES6',
+      level: '80'
+    },
+    {
+      name: 'WAMP',
+      level: '82'
     }
   ],
-  skillDescription: 'Also proficient in Adobe Photoshop and Illustrator, grew up bilingual (English and Klingon).',
+  skillDescription: 'Also proficient in Adobe Photoshop. I am fluent in English and Portuguese',
   contact: {
-    email: 'john.doe@email.com',
-    phone: '0123 456789',
-    street: '1234 Broadway',
-    city: 'New York',
-    website: 'johndoe.com',
-    github: 'johnyD'
+    email: 'yannunes@gmail.com',
+    phone: '+55 71 9 96427865',
+    github: 'midyan'
   },
   lang: 'en' // en, de, fr, pt, cn, it, es, th, pt-br, ru, sv, id, hu
 };
